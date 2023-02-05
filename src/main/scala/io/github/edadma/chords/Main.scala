dce50c21b733d28fdd74f4ea056164959ea724e3
Edward A Maxedon <edadma@gmail.com>
2023-02-04 20:24:40 -0500

    testing

--- src/main/scala/io/github/edadma/chords/Main.scala
+++ src/main/scala/io/github/edadma/chords/Main.scala
@@ -9,9 +9,34 @@ import io.github.edadma.libcairo._
   val cr = surface.create
 
   cr.setSourceRGB(0.0, 0.0, 0.0)
-  cr.selectFontFace("Georgia", FontSlant.NORMAL, FontWeight.BOLD)
+  cr.selectFontFace("zxcv", FontSlant.NORMAL, FontWeight.BOLD)
 
-  cr.setLineWidth(1)
+  cr.translate(36, 36)
+
+  cr.moveTo(0, 0)
+  cr.setLineWidth(.5)
+  cr.relLineTo(100, 0)
+  cr.stroke()
+
+  val fe = cr.fontExtents
+  val text = "12 point text"
+
+  cr.setFontSize(12)
+
+  val te = cr.textExtents(text)
+
+  val x = 0
+  val y = -te.yBearing
+
+  cr.moveTo(x, y)
+  cr.showText(text)
+  cr.moveTo(x, y + fe.height)
+  cr.showText(text)
+
+  cr.rectangle(x + te.xBearing, y + te.yBearing, te.width, te.height)
+  cr.stroke()
+
+  cr.moveTo(0, 0)
 
   for x <- 0 to 612 by 72 do
     cr.moveTo(x, 0)
